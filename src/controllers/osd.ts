8117c4dfd32c11fa3655242d5f056dc7684ba977
pust1n <justin686867@gmail.com>
2024-01-24 17:24:20 -0500

    Removed express from file

--- src/controllers/osd.ts
+++ src/controllers/osd.ts
@@ -2,7 +2,6 @@
 import xml from 'xml';
 import nconf from 'nconf';
 
-import { Request, Response } from 'express';
 import plugins from '../plugins';
 import meta from '../meta';
 
@@ -55,9 +54,9 @@ function generateXML(): string {
     }], { declaration: true, indent: '\t' });
 }
 
-type nextfunction = () => void
+type nextfunction = () => void;
 
-export default function handle(req : Request, res : Response, next: nextfunction) {
+export default function handle(req, res, next: nextfunction) {
     if (plugins.hooks.hasListeners('filter:search.query')) {
         res.type('application/opensearchdescription+xml').send(generateXML());
     } else {
